646ce6bb2fbc7fd6003615fdbedd6c11a2a2c42b
swyx <shawnthe1@gmail.com>
2021-01-16 20:46:38 +0800

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -104,7 +104,7 @@
 			{#each results as res}
 				<li>
 				{#if res.href.startsWith('/imgres')}
-					Image: <span><a href={res.href}>{res.href.slice(22,120) + '...'}</a></span>
+					Image: <span><a href={res.href.startsWith('/') ? "https://google.com" + res.href : res.href}>{res.href.slice(22,120) + '...'}</a></span>
 				{:else}
 					<h3><a href={res.href}>{res.linktext}</a></h3>
 					<span><a href={res.href}>{res.href}</a></span>
